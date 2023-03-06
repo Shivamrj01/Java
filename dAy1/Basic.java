7bc9bdfaf2c72b9cd0e1089ed80a3853d2da687b
Shivamrj01 <shivamraj982611@gmail.com>
2023-03-06 11:44:44 +0530

    first

--- dAy1/Basic.java
+++ dAy1/Basic.java
@@ -4,6 +4,12 @@ public class Basic{
 public static void main(String[] args) {
 
 System.out.println("Hello World Linux");
+
+int num = 80;
+int num1 = 101;
+
+System.out.println(num+num1-num-num1);
+
 }
 
 }
